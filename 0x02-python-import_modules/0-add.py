8e7badcb3216b1cf8eaa0b8b95fda61162a04796
prevail ugah <prevailbugah@gmail.com>
2023-03-12 14:37:19 -0700

    Fixed error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
-print(add(a, b))
+
+a = 1
+b = 2
+
+
+print(f"{a} + {b} = {add(a, b)}")
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-
-
 def add(a, b):
     result = a + b
-    print(f'{a} + {b} = {result}')
     return (result)
