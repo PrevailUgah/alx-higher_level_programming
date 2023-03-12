d33078b125c92ab3a298899122deaaff0a68eedf
prevail ugah <prevailbugah@gmail.com>
2023-03-12 14:07:27 -0700

    program that Imports a simple function from a simple file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from add_0 import add
+print(add(a, b))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+a = 1
+b = 2
+def add(a, b):
+    result = a + b
+    print(f'{a} + {b} = {result}')
+    return (result)
+
