03415bf5c97212c0c177491c6d8eb780b9ff6c14
prevail ugah <prevailbugah@gmail.com>
2023-03-13 16:34:50 -0700

    my loop list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 my_list = [1, 2, 3, 4, 5]
-for i in my_list:
-    print("{}".format(i))
+
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{}".format(i))
