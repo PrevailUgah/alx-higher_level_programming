bbdb9d9a64983c32a6478a1c096bc0864053e192
prevail ugah <prevailbugah@gmail.com>
2023-03-13 16:42:58 -0700

    Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
+# 0-print_list_integer.py
 
 
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
+
